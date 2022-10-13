bbd48aed1263f2f2a8b948a92cb7b35f4bdff9d1
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-10-14 01:51:25 +0900

    fix: export error fix

--- next.config.js
+++ next.config.js
@@ -2,13 +2,32 @@
 const withVideos = require('next-videos');
 
 const nextConfig = {
+  assetPrefix: '.',
   reactStrictMode: true,
   swcMinify: true,
 
-  i18n: {
-    locales: ["ko"],
-    defaultLocale: "ko",
+  // i18n: {
+  //   locales: ["ko"],
+  //   defaultLocale: "ko",
+  // },
+
+  images: {
+    loader: 'akamai',
+    path: '',
   },
+
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/about': { page: '/about' },
+  //     '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+  //     '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+  //     '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+  //   }
+  // },
 
   webpack(config, { isServer }) {
     config.module.rules.push({
