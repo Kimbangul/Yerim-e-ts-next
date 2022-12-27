dee9e3d0a1f8771d6ba129f1c7de58534cff14d7
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-12-28 00:40:41 +0900

    chore: axios 설치

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,6 @@
+import React, { useState, useEffect, ReactNode } from 'react';
+import { AxiosResponse } from 'axios';
+
 const onClickLinkBtn = (link: string, option?: string) => {
   if (option) {
     window.open(link, option);
@@ -6,5 +9,35 @@ const onClickLinkBtn = (link: string, option?: string) => {
   }
   return;
 };
+
+const useAPIcall = (callable:any, url: string, params: {[key:string]:any}) => {
+  const [state, setState] = useState('idle');
+  const [data, setData] = useState(null);
+  console.log(callable, url, params);
+
+  useEffect(() => {
+    callable(url, params).then((response : AxiosResponse) => {
+      console.log(response);
+      if (response.data.status === "SUCCESS") {
+        setData(data);
+        setState('fullfilled');
+
+      } else {
+        setState("rejected");
+
+      }
+    }).catch(() => {
+      setState("rejected");
+    })
+  }, [url, params]);
+
+
+  const LoadingPage = () => {
+    return;
+  }
+
+  return {   state, data, setData, LoadingPage  }
+}
+
 
 export { onClickLinkBtn };
