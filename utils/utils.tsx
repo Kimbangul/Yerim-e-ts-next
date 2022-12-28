7509483101a7880f78971520cc21b9b64b98b2b6
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-12-28 15:07:41 +0900

    style: useAPICall util function add

---
+++ utils/utils.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, ReactNode } from 'react';
+import { AxiosResponse } from 'axios';
+
+const onClickLinkBtn = (link: string, option?: string) => {
+  if (option) {
+    window.open(link, option);
+  } else {
+    window.open(link);
+  }
+  return;
+};
+
+const useAPIcall = (
+  callable: any,
+  url: string,
+  params: { [key: string]: any }
+) => {
+  const [state, setState] = useState('idle');
+  const [data, setData] = useState(null);
+  console.log(callable, url, params);
+
+  useEffect(() => {
+    callable(url, params)
+      .then((response: AxiosResponse) => {
+        console.log(response);
+        if (response.data.status === 'SUCCESS') {
+          setData(data);
+          setState('fullfilled');
+        } else {
+          setState('rejected');
+        }
+      })
+      .catch(() => {
+        setState('rejected');
+      });
+  }, [url, params]);
+
+  const LoadingPage: React.FC = () => {
+    return <div>Loading</div>;
+  };
+
+  return { state, data, setData, LoadingPage };
+};
+
+export { onClickLinkBtn, useAPIcall };
