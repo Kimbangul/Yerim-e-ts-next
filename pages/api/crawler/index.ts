c68ad836d75e4d94180d0f386d3a474ff65491b3
Kimbangul <highcolor_12@naver.com>
2023-06-29 08:02:59 +0900

    feat: add crawler

--- pages/api/crawler/index.ts
+++ pages/api/crawler/index.ts
@@ -1,10 +1,56 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import cheerio, {Element} from 'cheerio';
+import { ContentType } from './type';
+
+
+const getHtml = async (url : string) => {
+  try {
+    const html = await axios.get(url);
+    const $ = cheerio.load(html.data);
+
+    let content : ContentType[] = [];
+    const ARTICLE_SELECTOR = $("#root div:nth-child(2) div:nth-child(3) div:nth-child(4) div:nth-child(3) > div > div");
+
+    // FUNCTION get tag
+    const getTag = (tagSelector : Element) => {
+      let result : string[] = []
+      
+      const tagList = $(tagSelector).find(".tags-wrapper > a");
+
+      tagList.map((idx,el)=>{
+        const tag = $(el).text();
+        result[idx] = tag;
+      });
+    
+      return result;
+    }
+
+    ARTICLE_SELECTOR.map((idx, el) => {
+      content[idx] = {
+        head: $(el).find("img").attr('src'),
+        date: $(el).find(".subinfo > span:first-of-type").text(),
+        context: $(el).find("p").text(),
+        href: $(el).find("a:first-child").attr('href'),
+        headline: $(el).find("h2").text(),
+        tags: getTag(el),
+      }
+      
+    });
+    console.log(content);
+    const json = JSON.stringify(content);
+    return json;
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+
+const articles = (url : string) => getHtml(url);
 
 export default function handler (
   req: NextApiRequest,
   res: NextApiResponse)  {
   
-      res.status(200).json({
-          sample: 'hello',
-       })
+      res.status(200).json(articles("https://velog.io/@kimbangul"))
 }
---
+++ pages/api/crawler/type.ts
@@ -0,0 +1,8 @@
+export interface ContentType {
+  head?: string;
+  date: string;
+  context: string;
+  href?: string;
+  headline: string;
+  tags: string[];
+}
