0ba54c63a7e33f80c8395b7739007044845d8b48
Kimbangul <highcolor_12@naver.com>
2023-07-07 08:04:43 +0900

    chore: build config edit

--- next.config.js
+++ next.config.js
@@ -3,21 +3,21 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  assetPrefix: isProduction ? 'https://kimbangul.github.io/' : null,
-  trailingSlash: true,
-  reactStrictMode: true,
-  swcMinify: true,
-
-  // basePath: isProduction ? '/.' : 'http://127.0.0.1:3000/',
+  // assetPrefix: isProduction ? 'https://kimbangul.github.io/' : null,
+  // trailingSlash: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  // basePath:'/.',
+  //basePath: isProduction ? '/.' : 'http://127.0.0.1:3000/',
 
   // i18n: {
   //   locales: ["ko"],
   //   defaultLocale: "ko",
   // },
-  images: isProduction ? {
-    loader: 'akamai',
-    path: '',
-  } : {},
+  // images: isProduction ? {
+  //   loader: 'akamai',
+  //   path: '',
+  // } : {},
 
   webpack(config, { isServer }) {
     config.module.rules.push({
