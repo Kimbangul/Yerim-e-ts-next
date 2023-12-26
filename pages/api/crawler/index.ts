712b084e5c36e906967fe462f66e64046b717042
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-12-26 23:12:44 +0900

    fix: edit crawler

--- pages/api/crawler/index.ts
+++ pages/api/crawler/index.ts
@@ -3,45 +3,37 @@ import axios from 'axios';
 import cheerio, {Element} from 'cheerio';
 import { ContentType } from './type';
 
-const openBrowser = async () => {
+const openBrowser  = async () => {
   const puppeteer = require('puppeteer');
 
   //1. 크로미움으로 브라우저를 연다. 
-  const browser = await puppeteer.launch(); // -> 여기서 여러가지 옵션을 설정할 수 있다.
+  const browser = await puppeteer.launch({
+    headless: true
+  }); // -> 여기서 여러가지 옵션을 설정할 수 있다.
         
   //2. 페이지 열기
   const page = await browser.newPage();
         
   //3. 링크 이동
-  await page.goto(`${process.env.NEXT_PUBLIC_BLOG_URL || ''}`);
+  await page.goto(`${process.env.NEXT_PUBLIC_BLOG_URL || ''}`, {
+    waitUntil: "networkidle2" // 500ms 동안 두 개 이상의 네트워크 연결이 없을 때 탐색이 완료되는 것으로 간주
+  });
 
   //4. HTML 정보 가지고 온다.
-  const content = await page.content();
-
-  console.log(content);
+const content  : string= await page.content();
           
   //5. 페이지와 브라우저 종료
   await page.close();
+
+  return content;
 }
 
 const getHtml = async (url : string) => {
-  console.log(url);
-  try {
-    const html = await axios.get(url);
-    const $ = cheerio.load(html.data);
-   // console.log($);
+  try {  
+    const $ = cheerio.load(await openBrowser());
 
     let content : ContentType[] = [];
-    //const ARTICLE_SELECTOR = $("#root div:nth-child(2) div:nth-child(3) div:nth-child(4) div:nth-child(3) > div > div");
-    //const ARTICLE_SELECTOR = $(".FlatPostCardList_block__VoFQe > div");
     const ARTICLE_SELECTOR= $("main section > div:nth-child(2) > div:nth-child(3) > div");
-    //const Selector = $("main section:first-of-type > div:nth-child(2) > div:nth-child(3)");
-   // const classNm = $(".FlatPostCard_block__a1qM7");
-    console.log($(".FlatPostCard_block__a1qM7").length);
-    console.log($("main section > div:nth-child(2) > div:nth-child(3)").find('p').text());
-    console.log($(".FlatPostCard_block__a1qM7"));
- //   console.log(classNm.length);
-  //  console.log(classNm[0]);
 
     // FUNCTION get tag
     const getTag = (tagSelector : Element) => {
@@ -59,12 +51,6 @@ const getHtml = async (url : string) => {
 
     ARTICLE_SELECTOR.map((idx, el) => {     
       content[idx] = {
-        // head: $(el).find("img").attr('src'),
-        // date: $(el).find(".subinfo > span:first-of-type").text(),
-        // context: $(el).find("p").text(),
-        // href: $(el).find("a:first-child").attr('href'),
-        // headline: $(el).find("h2").text(),
-        // tags: getTag(el),
         head: $(el).find("img").attr('src'),
         date: $(el).find(".FlatPostCard_subInfo__cT3J6 > span:first-of-type").text(),
         context: $(el).find("p").text(),
@@ -73,7 +59,7 @@ const getHtml = async (url : string) => {
         tags: getTag(el),
       };
     });
-    
+    console.log(content);
     return content;
   }
   catch(e){
@@ -85,9 +71,6 @@ const article = getHtml(process.env.NEXT_PUBLIC_BLOG_URL || '');
 
 export default async function handler (
   req: NextApiRequest,
-  res: NextApiResponse)  {
- // console.log(article);
-  //res.status(200).json(await article)
-  openBrowser();
+  res: NextApiResponse)  {  
   res.status(200).json(await article);
 }
