9de92531718185b5cc02f6b8dd5c3146a2de54ff
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-12-28 21:14:33 +0900

    test: edit test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { NextPage } from 'next';
 import { useEffect, useMemo } from 'react';
 import useApiCall from 'utils/useApiCall';
-import PostList from 'src/component/Posts/PostsList';
+import PostView from 'src/component/Posts/PostsView';
 
 // COMPONENT main component
 const Home: NextPage = () => {
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
   return (
     <>
     <h1>test page</h1>
-    <PostList />
+    <PostView />
     </>
   );
 };
