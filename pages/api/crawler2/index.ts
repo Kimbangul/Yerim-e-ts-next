45c4f81cb80780949dd07eae9c9896af9e45be82
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-12-31 02:33:10 +0900

    feat: add velog api

--- pages/api/crawler2/index.ts
+++ pages/api/crawler2/index.ts
@@ -1,51 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import cheerio, {Element} from 'cheerio';
 import { ContentType } from '../crawler/type';
+import axios from 'axios';
 
 
 // const article = getHtml(process.env.NEXT_PUBLIC_BLOG_URL || '');
 const getHtml = async (url : string) => {
-  console.log(url);
+  //console.log(url); 
   try {  
-    const $ = cheerio.load(url);
-    console.log($);
-
-    let content : any[] = [];
-    const ARTICLE_SELECTOR= $("main section > div:nth-child(2) > div:nth-child(3) > div");
-
-    // FUNCTION get tag
-    const getTag = (tagSelector : Element) => {
-      let result : string[] = []
-      
-      const tagList = $(tagSelector).find(".FlatPostCard_tagsWrapper__iNQR3 > a");
-
-      tagList.map((idx,el)=>{
-        const tag = $(el).text();
-        result[idx] = tag;
-      });
-    
-      return result;
-    }
-
-    ARTICLE_SELECTOR.map((idx, el) => {     
-      content[idx] = {
-        head: $(el).find("img").attr('src'),
-        date: $(el).find(".FlatPostCard_subInfo__cT3J6 > span:first-of-type").text(),
-        context: $(el).find("p").text(),
-        href: $(el).find("a:first-child").attr('href'),
-        headline: $(el).find("h2").text(),
-        tags: getTag(el),
-      };
+    const html = await axios({
+      url: "https://v2.velog.io/graphql",
+      method: "post",
+      data: {
+        query: `
+            query Posts($cursor: ID, $username: String, $temp_only: Boolean, $limit: Int) {
+                posts(cursor: $cursor, username: $username, temp_only: $temp_only, limit: $limit) {
+                  id
+                  title
+                  short_description
+                  thumbnail
+                  user {
+                    username
+                    profile {
+                      thumbnail
+                    }
+                  }
+                  url_slug
+                  released_at
+                  updated_at
+                  comments_count
+                  tags
+                  likes
+                }
+              }
+            `,
+        variables:{
+              username: 'kimbangul',
+              limit: 4,
+            }
+      }
     });
-    //console.log(content);
-    content.push($);
-    return content;
+    //console.log(html.data.data.posts);
+    return html.data.data.posts;
   }
   catch(e){
     console.log(e);
   }
 }
-const article = getHtml(process.env.NEXT_PUBLIC_BLOG_URL || '');
+const article =  getHtml(process.env.NEXT_PUBLIC_BLOG_URL || '');
+console.log(article);
 
 export default async function handler (
   req: NextApiRequest,
