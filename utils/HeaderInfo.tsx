ba45536d26f040ef7644128ab8fc8513d0fbe49a
Kimbangul <highcolor_12@naver.com>
2024-03-28 16:43:27 +0900

    style: edit contents

--- utils/HeaderInfo.tsx
+++ utils/HeaderInfo.tsx
@@ -8,12 +8,12 @@ const HeaderInfo = () => {
       <meta charSet='UTF-8' />
       <meta name="google-site-verification" content="YxlqZ71uNOrnU5b6IEnjMCb7CYtdr-LeWdgBl6WlY5o" />
       <meta property='og:url' content='https://yerim-e.com/' />
-      <meta property='og:title' content='YERIM.e :: 박예림 - 웹 퍼블리셔 포트폴리오' />
+      <meta property='og:title' content='YERIM.e :: 박예림 - 프론트엔드 개발자 포트폴리오' />
       <meta property='og:type' content='website' />
       <meta property='og:image' content='https://yerim-e.com/image/og-image.jpg'/>
       <meta
         property='og:description'
-        content='웹 퍼블리셔, 웹 개발자, UI 개발자 박예림 포트폴리오입니다.'
+        content='프론트엔드 개발자, 웹 퍼블리셔, 웹 개발자, UI 개발자 박예림 포트폴리오입니다.'
       />
       <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
       <link rel='icon' href='/favicon.png' type='image/x-icon' />
