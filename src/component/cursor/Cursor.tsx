86e296f869450ee423504f260cc7c9b2be0d1853
Kimbangul <highcolor_12@naver.com>
2024-04-17 09:51:09 +0900

    feat: add useThrottle

--- src/component/cursor/Cursor.tsx
+++ src/component/cursor/Cursor.tsx
@@ -1,40 +1,48 @@
 'use client';
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import useThrottle from '@/hook/useThrottle';
 
 const Cursor = () => {
   const bodyRef = useRef<HTMLBodyElement>();
   const [xy, setXY] = useState({ x: 0, y: 0 });
 
   const onMoveMouse = (e: MouseEvent) => {
-    console.log(e);
+    console.log(e.clientX, e.clientY);
     setXY({ x: e.clientX, y: e.clientY });
   };
+
+  const throttleOnMove = useThrottle<[MouseEvent]>(onMoveMouse, 30);
 
   useEffect(() => {
     bodyRef.current = document.querySelector('body') || undefined;
 
     if (bodyRef.current) {
-      bodyRef.current.addEventListener('mousemove', onMoveMouse);
+      console.log('add');
+      bodyRef.current.addEventListener('mousemove', throttleOnMove);
     }
 
     return () => {
       if (bodyRef.current) {
-        bodyRef.current.removeEventListener('mousemove', onMoveMouse);
+        console.log('remove');
+        bodyRef.current.removeEventListener('mousemove', throttleOnMove);
       }
     };
   }, []);
 
-  return <CursorStyle $x={xy.x} $y={xy.y} />;
+  return <CursorStyle.Cursor style={{ transform: `translate(${xy.x}px, ${xy.y}px)` }} />;
 };
 
-const CursorStyle = styled.div<{ $x: number; $y: number }>`
-  position: absolute;
-  width: 5rem;
-  height: 5rem;
-  background: rgba(255, 255, 255, 0.2);
-  border-radius: 100%;
-  /* transform: translate(${props => props.$x}px, ${props => props.$y}px); */
-`;
-
+const CursorStyle = {
+  Cursor: styled.div`
+    position: fixed;
+    z-index: 999;
+    width: 5rem;
+    height: 5rem;
+    margin: -2.5rem -5rem 0 0;
+    pointer-events: none;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 100%;
+  `,
+};
 export default Cursor;
