8b0f06102f1faec66f352122a9425aa625765f82
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2024-05-09 13:05:25 +0900

    refactor: dynamic import로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,25 @@
 import { Main, About, Tech, Contact, Post, Design, Work, Modal } from '@/component/layout/index';
 import ObserverProvider from '@/component//observer/ObserverProvider';
 
+const page = [
+  <ObserverProvider key="main">
+    <Main />
+  </ObserverProvider>,
+  <About key="about" />,
+  <Tech key="tech" />,
+  <Work key="work" />,
+  <Design key="design" />,
+  <ObserverProvider key="post">
+    <Post />
+  </ObserverProvider>,
+  <ObserverProvider key="contact">
+    <Contact />
+  </ObserverProvider>,
+];
+
 export default function Home() {
-  const page = [
-    <ObserverProvider key="main">
-      <Main />
-    </ObserverProvider>,
-    <About key="about" />,
-    <Tech key="tech" />,
-    <Work key="work" />,
-    <Design key="design" />,
-    <ObserverProvider key="post">
-      <Post />
-    </ObserverProvider>,
-    <ObserverProvider key="contact">
-      <Contact />
-    </ObserverProvider>,
-  ];
   return (
     <>
-      {/* {page.map((el, idx) => (
-        <ObserverProvider key={`section${idx}${el.key}`}>{el}</ObserverProvider>
-      ))} */}
       {page.map((el, idx) => el)}
       <Modal />
     </>
--- src/component/layout/index.ts
+++ src/component/layout/index.ts
@@ -1,11 +1,19 @@
+import dynamic from 'next/dynamic';
+
 export { default as Header } from '@/component/layout/header/HeaderContainer';
 export { default as Main } from '@/component/layout/main/MainContainer';
-export { default as About } from '@/component/layout/about/AboutContainer';
-export { default as Tech } from '@/component/layout/tech/TechContainer';
+export const About = dynamic(() => import('@/component/layout/about/AboutContainer'));
+// export { default as About } from '@/component/layout/about/AboutContainer';
+export const Tech = dynamic(() => import('@/component/layout/tech/TechContainer'));
+// export { default as Tech } from '@/component/layout/tech/TechContainer';
 export { default as Footer } from '@/component/layout/footer/FooterContainer';
-export { default as Contact } from '@/component/layout/contact/ContactContainer';
-export { default as Post } from '@/component/layout/post/PostContainer';
-export { default as Design } from '@/component/layout/design/DesignContainer';
-export { default as Work } from '@/component/layout/work/WorkContainer';
+// export { default as Contact } from '@/component/layout/contact/ContactContainer';
+export const Contact = dynamic(() => import('@/component/layout/contact/ContactContainer'));
+// export { default as Post } from '@/component/layout/post/PostContainer';
+export const Post = dynamic(() => import('@/component/layout/post/PostContainer'));
+// export { default as Design } from '@/component/layout/design/DesignContainer';
+export const Design = dynamic(() => import('@/component/layout/design/DesignContainer'));
+// export { default as Work } from '@/component/layout/work/WorkContainer';
+export const Work = dynamic(() => import('@/component/layout/work/WorkContainer'));
+export const Modal = dynamic(() => import('@/component/common/modal/ModalContainer'));
 export { default as Custom404 } from '@/component/layout/404/404View';
-export { default as Modal } from '@/component/common/modal/ModalContainer';
